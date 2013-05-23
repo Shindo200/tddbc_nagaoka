b1e1b84fa68ed02a76590330a3492d08383219d2
Suguru Odai <ourqwers@gmail.com>
2013-05-24 00:51:35 +0900

    #next_critical_patch_updateのテストを追加

--- spec/lib/version_spec.rb
+++ spec/lib/version_spec.rb
@@ -94,5 +94,12 @@ describe "Version" do
         @f8u05.next_limited_update.update_number.should eq 20
       end
     end
+
+    describe "#next_critical_patch_update" do
+      it "次のLimitedUpdateの番号を持つVersionを返すこと" do
+        @f7u40.next_critical_patch_update.update_number.should eq 45
+        @f8u05.next_critical_path_update.update_number.should eq 11
+      end
+    end
   end
 end
